9544d0fae1d1f2d55988d2837f9fe1637307d43b
dirkliu <liuqi0702@126.com>
2016-08-29 18:06:25 +0800

    autoprefix watch

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,23 @@ var gulp = require('gulp');
 
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var autoprefix =require('gulp-autoprefixer');
 var del=require('del');
 
 gulp.task('default',function(){
     gulp.src('js/*.js')
-        .pipe(uglify('all.js'))
+        .pipe(uglify())
         .pipe(concat('all.js'))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist/js'));
 });
+
+gulp.task('autoprefix',function(){
+    return gulp.src('css/*.css').pipe(autoprefix({browers:['last 4 versions']})).pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('watch',function(){
+    gulp.watch('css/*.css',['autoprefix']);
+})
 
 gulp.task('clean', function() {
     return del(['dist/*']);
