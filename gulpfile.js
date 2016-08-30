991ae652817b4433343a337f718c81c23ca6dc54
dirkliu <liuqi0702@126.com>
2016-08-30 19:40:27 +0800

    sass,useref

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var autoprefix =require('gulp-autoprefixer');
+var useref=require('gulp-useref');
+var gulpif=require('gulp-if');
+var sass=require('gulp-sass');
 var del=require('del');
 
 gulp.task('default',function(){
@@ -18,8 +21,22 @@ gulp.task('autoprefix',function(){
 
 gulp.task('watch',function(){
     gulp.watch('css/*.css',['autoprefix']);
+    gulp.watch('css/*.scss',['sass']);
 })
 
 gulp.task('clean', function() {
     return del(['dist/*']);
+});
+
+gulp.task('html',function(){
+    gulp.src('*.html')
+        .pipe(useref())
+        .pipe(gulpif('*.js',uglify()))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('sass',function(){
+    gulp.src('css/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist/css'))
 });
