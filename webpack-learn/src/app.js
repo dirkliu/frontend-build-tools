2f9027ec5c58cd0a9701569ab2decc1feaddccba
dirkliu <liuqi0702@126.com>
2016-10-10 19:08:29 +0800

    import angular!

--- webpack-learn/src/app.js
+++ webpack-learn/src/app.js
@@ -4,9 +4,26 @@
 import 'babel-polyfill';
 import cats from './cats';
 import $ from 'jquery';
+import angular from "angular";
 
-$('<h1>Cats</h1>').appendTo('body');
+/*$('<h1>Cats</h1>').appendTo('body');
 const ul = $('<ul></ul>').appendTo('body');
 for (const cat of cats) {
     $('<li></li>').text(cat).appendTo(ul);
+}*/
+
+/*var test=[1,2,3,4,5];
+angular.forEach(test,function(value){
+    console.log('test item:',value);
+});*/
+
+angular.module('app',[]).directive('app',[App]);
+
+function App(){
+    return {
+        restrict:"A",
+        link:function(scope,element,attr){
+            element.html('angular works!');
+        }
+    }
 }
