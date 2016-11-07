811dad1e1daf07a2da95513ac68f42fe6a541eb5
Liu7 <liuqi0702@126.com>
2016-11-07 23:18:55 +0800

    gulp react es6 merged!

--- gulp-learn/js/a.js
+++
@@ -1,7 +0,0 @@
-(function(){
-   'use strict';
-    var a='test a';
-    console.log('test a:',a);
-    var test;
-    test=+a;
-})();
--- gulp-learn/js/b.js
+++
@@ -1,6 +0,0 @@
-(function(){
-    var b='test b';
-    console.log('test b:',b);
-    var test;
-    test=+b;
-})();
