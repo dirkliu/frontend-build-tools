3305db23bebcdd6c7a8f3479c08e1f05abb3dda0
liuqi <liuqi0702@126.com>
2016-11-25 19:13:20 +0800

    install gulp-connect

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -2,40 +2,50 @@ var gulp = require('gulp');
 
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
-var autoprefix =require('gulp-autoprefixer');
-var gulpif=require('gulp-if');
-var sass=require('gulp-sass');
-var livereload=require('gulp-livereload');
-var del=require('del');
-var http=require('http');
-var st=require('st');
+var autoprefix = require('gulp-autoprefixer');
+var gulpif = require('gulp-if');
+var sass = require('gulp-sass');
+var livereload = require('gulp-livereload');
+var del = require('del');
+var http = require('http');
+var st = require('st');
+var open = require('gulp-open');
 
-gulp.task('clean', function() {
+gulp.task('clean', function () {
     return del(['dist']);
 });
 
-gulp.task('sass',function(){
+gulp.task('sass', function () {
     gulp.src('styles/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('dist/styles'))
-        .pipe(livereload());
+        .pipe(livereload({ start: true }));
 });
 
-gulp.task('html',function(){
+gulp.task('html', function () {
     return gulp.src('index.html')
         .pipe(gulp.dest('dist'))
-        .pipe(livereload());
+        .pipe(livereload({ start: true }));
 });
 
-gulp.task('server',function(done){
+gulp.task('server', function (done) {
     http.createServer(
-        st({ path: __dirname + '/dist', index: 'index.html', cache: false })
+        st({path: __dirname + '/dist', index: 'index.html', cache: false})
     ).listen(8080, done);
 });
 
-gulp.task('watch', function() {
-    livereload.listen({ basePath: 'dist' });
+gulp.task('watch', function () {
+    livereload.listen();
     gulp.watch('styles/*.scss', ['sass']);
+    gulp.watch('*.html', ['html']);
 });
 
-gulp.task('serve',['clean','sass','html','server','watch']);
+gulp.task('open', function () {
+    gulp.src('./')
+        .pipe(open({
+            uri:'http://localhost:8080',
+            app:'chrome'
+        }))
+});
+
+gulp.task('serve', ['clean', 'sass', 'html', 'server','open','watch']);
