30de3f61b8dc7e9e8f1c41c034cb6893058c1ab9
Liu7 <liuqi0702@126.com>
2016-11-26 12:18:25 +0800

    gulp-connect!

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -5,10 +5,9 @@ var concat = require('gulp-concat');
 var autoprefix = require('gulp-autoprefixer');
 var gulpif = require('gulp-if');
 var sass = require('gulp-sass');
-var livereload = require('gulp-livereload');
 var del = require('del');
-var http = require('http');
 var st = require('st');
+var connect=require('gulp-connect');
 var open = require('gulp-open');
 
 gulp.task('clean', function () {
@@ -19,23 +18,23 @@ gulp.task('sass', function () {
     gulp.src('styles/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('dist/styles'))
-        .pipe(livereload({ start: true }));
+        .pipe(connect.reload());
 });
 
 gulp.task('html', function () {
     return gulp.src('index.html')
         .pipe(gulp.dest('dist'))
-        .pipe(livereload({ start: true }));
+        .pipe(connect.reload());
 });
 
 gulp.task('server', function (done) {
-    http.createServer(
-        st({path: __dirname + '/dist', index: 'index.html', cache: false})
-    ).listen(8080, done);
+    connect.server({
+        root:'dist',
+        livereload: true
+    });
 });
 
 gulp.task('watch', function () {
-    livereload.listen();
     gulp.watch('styles/*.scss', ['sass']);
     gulp.watch('*.html', ['html']);
 });
