2b865149861701cd953741350d0520784c493c05
Liu7 <liuqi0702@126.com>
2016-12-09 23:30:17 +0800

    require('./test');插件试写！

--- webpack/constants-replace-plugin.js
+++ webpack/constants-replace-plugin.js
@@ -1,12 +1,24 @@
-var fs=require('fs');
-
 function ConstantsReplacePlugin(options){
-    this.options=options;
+    this.options=options||{};
 }
 
 ConstantsReplacePlugin.prototype.apply = function(compiler) {
     compiler.plugin('done', function() {
+        console.log('Welcome,constants-replace-compiler done!');
+    });
 
+    compiler.plugin("compilation", function(compilation) {
+        console.log('constants-replace,start compilation');
+
+
+        compilation.plugin('seal', function() {
+            console.log('compilation,seal');
+        });
+        compilation.plugin('normal-module-loader', function(loaderContext, module) {
+            //this is where all the modules are loaded
+            //one by one, no dependencies are created yet
+            console.log('compilation,normal-module-loader');
+        });
     });
 };
 
