08002a907d81da1d9c8870fc3172d67697b8a9ea
dirkliu <liuqi0702@126.com>
2017-06-03 23:16:52 +0800

    webpack cli

---
+++ webpack-learn/build.js
@@ -0,0 +1,74 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+webpack(
+  {
+      entry: {
+          'index': ['./src/index.js'],
+          'article': './src/article.js',
+          'vendor': ['./src/common.js']
+      },
+      output: {
+          path: './dist',
+          filename: '[name].[hash].js',
+          chunkFilename: '[name].[hash].js'
+      },
+
+      module: {
+          rules: [
+              /*{
+                  test: /\.html$/,
+                  loader: 'raw'
+              }*/, {
+                  test: /\.js$/,
+                  exclude: /node_modules/,
+                  loader: 'babel-loader'
+              }, {
+                  test: /\.scss$/,
+                  loaders: ["style", "css", "resolve-url", "sass"]
+              }, {
+                  test: /\.(jpe?g|png|gif|svg)$/i,
+                  loader: 'url!resolve-url!img?progressive=true'
+              }
+          ]
+      },
+
+      plugins: [
+          new HtmlWebpackPlugin({
+              filename: 'index.html',
+              template: './index.html',
+              title: 'Index page',
+              favicon: './favicon.ico',
+              chunks: ['vendor', 'index']
+          }),
+          new HtmlWebpackPlugin({
+              filename: 'article.html',
+              template: './article.html',
+              title: 'Article page',
+              favicon: './favicon.ico',
+              chunks: ['vendor', 'article']
+          }),
+          /*new webpack.optimize.UglifyJsPlugin({
+           compress: {
+           warnings: false,
+           },
+           output: {
+           comments: false,
+           }
+           }),*/
+          new webpack.DefinePlugin({
+              CONSTANT_A:branch
+          }),
+          new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.[hash].js'),
+          new webpack.HotModuleReplacementPlugin()
+      ],
+
+      devServer: {
+          contentBase: './bin',
+          port: 8000,
+          inline: true,
+          open: 'http://localhost:8000/#/'
+      },
+      devtool: "eval"
+  }
+)
