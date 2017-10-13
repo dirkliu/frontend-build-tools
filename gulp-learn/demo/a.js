9fd07ebaf63929f4c47b93add16ee645b3b520c9
dirkliu <liuqi0702@126.com>
2017-10-13 21:39:53 +0800

    delete demo

--- gulp-learn/demo/a.js
+++
@@ -1,4 +0,0 @@
-console.log('this is a.js')
-console.log('this is a.js 1')
-console.log('this is a.js 2')
-console.log('this is a.js 3')
--- gulp-learn/demo/b.js
+++
@@ -1,4 +0,0 @@
-console.log('this is b.js')
-console.log('this is b.js 1')
-console.log('this is b.js 2')
-console.log('this is b.js 3')
