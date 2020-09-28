c5bcc61640359c740b9ff3d284eb32832095fe0b
dirkliu <liuqi0702@126.com>
2020-09-28 23:09:54 +0800

    add tsconfig

--- typescript/src/greeter.ts
+++ typescript/src/greeter.ts
@@ -3,12 +3,8 @@ interface Person {
   lastName: string;
 }
 
-function greeter (person: Person):string{
+function greeter(person: Person):string{
   return "Hello, " + person.firstName + " " + person.lastName;
-}
-
-function add(x: number, y: number): number {
-  return x + y;
 }
 
 var user = { firstName: "Jane", lastName: "User" };
