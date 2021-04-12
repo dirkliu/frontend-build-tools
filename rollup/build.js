4516d3c3532c6e62cd84c4dbdb119d3258fd0a79
dirkliu <liuqi0702@126.com>
2021-04-12 17:03:08 +0800

    add rollup testplugin!

--- rollup/build.js
+++ rollup/build.js
@@ -1,11 +1,16 @@
-var rollup = require("rollup");
-var babel = require("rollup-plugin-babel");
+
+const rollup = require("rollup")
+const babel = require("rollup-plugin-babel")
+const myTest = require('./myTest')
 
 async function build() {
   // create a bundle
   const bundle = await rollup.rollup({
     input: "./src/index.js",
     plugins: [
+      myTest({
+        // include: '*.svg'
+      }),
       babel({
         exclude: 'node_modules/**' // 只编译我们的源代码
       })
